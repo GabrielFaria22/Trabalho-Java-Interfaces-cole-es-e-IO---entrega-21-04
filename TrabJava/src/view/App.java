99d5fb0c26e8d38d2377504718929dd08af0cb53
Gabriel Faria <42048534+GabrielFaria22@users.noreply.github.com>
2019-04-12 15:13:24 -0300

    Add files via upload

---
+++ TrabJava/src/view/App.java
@@ -0,0 +1,13 @@
+package view;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		MenuPrincipal m=new MenuPrincipal();
+		
+		m.menu();
+
+	}
+
+}
--- TrabJava/src/view/MenuPrincipal.java
+++ TrabJava/src/view/MenuPrincipal.java
@@ -14,20 +14,14 @@ public class MenuPrincipal {
 		
 		int opcao = 0;
 		boolean respostavalida=false;
-		while(respostavalida==false) {
 			System.out.println("Bem vindo ao Sistema de Consulta de Chamado");
 			System.out.println("Digite 1 para consultar");
 			System.out.println("Digite 2 para salvar");
 			System.out.println("Digite 3 para carregar");
 			System.out.println("Digite 4 para sair");
 			
-			try {
-				opcao=leitor.nextInt();
-				respostavalida=true;
-			}catch(RuntimeException a) {//na vdd provavel que precise ser ioexception
-				System.out.println("Erro, favor digitar novamente");
-			}
-		}
+			opcao=leitor.nextInt();
+			
 		return opcao;
 		
 		
